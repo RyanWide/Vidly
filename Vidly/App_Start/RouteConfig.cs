d2fb330a7b6a4abcf7406b894481e29a8fca2278
RyanWide <61987678+RyanWide@users.noreply.github.com>
2020-03-17 09:26:21 -0700

    init commit

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,17 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            ////for custom routes, need line: however need to change action manually for change in controller
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",//name
+            //    "movies/released/{year}/{month}",//url
+            //    new { controller = "Movies", action = "ByReleaseDate" },//controller mapping
+            //    new { year = @"\d{4}" , month = @"\d{2}"}//restriction inform of regular expression, or do ####|#### to set allowed value
+            //);
+
+            //cleaner way to do custom routes using attribute:
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+
+            //Types of action views:
+            //return Content("hello world");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new {page =1, sortBy = "name}); //get calls to the new part of arg 
+        }
+        
+        //attribute route example:
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        // GET: Movies/Edit
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);  
+        }
+
+        //GET: Movies/Index or Movies (b/c its index type method)
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            //perimitive type like int need ? for nullable calls
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy{1}", pageIndex, sortBy));
+        }
+    }
+}
