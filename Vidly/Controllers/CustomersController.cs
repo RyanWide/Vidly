56646dec23095e26ccb2fbf94177a2f1a2d8bfcb
RyanWide <61987678+RyanWide@users.noreply.github.com>
2020-03-18 09:39:19 -0700

    section 4: 55

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -36,7 +36,7 @@ namespace Vidly.Controllers
             var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes,
-
+                Customer = new Customer(),
             };
             return View("CustomerForm", viewModel);
         }
@@ -53,6 +53,15 @@ namespace Vidly.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
             if(customer.Id == 0)
                 _context.Customers.Add(customer);
             else
